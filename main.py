19b42148098c08876bfc776504f3f922b396e01e
ntarsillo <nicholastarsillo@nicholass-mbp.mynetworksettings.com>
2024-09-25 18:28:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+import discord
+
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="parsawar/profanity_model_3.1")
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(intents=discord.Intents.default())
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    should_filter = pipe.predict(message.content)[
+        0]['label'] == '1'
+    if should_filter:
+        message.delete()
+
+client.run(TOKEN)
